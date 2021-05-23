bb4a35164b02d01896ea743573555920873e710c
Petryk Roman <romic.argument@gmail.com>
2021-05-23 13:21:17 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,20 +3,15 @@ const inputValue = document.querySelector('input#validation-input');
 let totalSymbol = parseInt(inputValue.getAttribute('data-length'), 10);
 
 inputValue.addEventListener('blur', () => {
-    if (totalSymbol !== inputValue.value.length) {
+    if (totalSymbol === inputValue.value.length) {
+        inputValue.classList.add('valid')
+    }
+    else if (totalSymbol !== inputValue.value.length) {
         inputValue.classList.add('invalid')
-    } else if (totalSymbol === inputValue.value.length) { }
-    inputValue.classList.add('valid')
+    }
 });
    
 inputValue.addEventListener('focus', () => {
     inputValue.classList.remove('invalid', 'valid')
-} )
+});
 
-
-// inputValue.addEventListener('input', onInputChenge)
-
-// function onInputChenge(event) {
-//     if(event.currentTarget.value.length === )
-    // youName.innerHTML= `${event.currentTarget.value}`
-// }
