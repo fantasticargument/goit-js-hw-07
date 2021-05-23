c09909cb27cdda3aa2e5be809a3aacb8a6cbe5a6
Petryk Roman <romic.argument@gmail.com>
2021-05-23 13:56:54 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,6 @@
 const input = document.querySelector('input#name-input');
 const youName = document.querySelector('span#name-output');
 
-input.addEventListener('input', onInputChenge)
-
-function onInputChenge(event) {
-    youName.innerHTML= `${event.currentTarget.value}`
-}
+input.addEventListener('input', (event) => {
+    youName.innerHTML = `${event.currentTarget.value}`
+});
