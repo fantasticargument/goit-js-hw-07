e74e605461ea5cb661289f8f505523fa74954bb8
Petryk Roman <romic.argument@gmail.com>
2021-05-24 18:36:22 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,5 @@
 const slider = document.querySelector('input#font-size-control');
-const size = document.querySelector('span#text')
+const size = document.querySelector('span#text');
 
 slider.addEventListener('input', () => {
     size.style.fontSize = `${slider.value}px`;
